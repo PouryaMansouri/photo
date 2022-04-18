0fce25e52524cadc2b1911e87f7014361bbe12c1
kiarashfz <kiawfz3673@gmail.com>
2022-04-18 07:01:57 +0430

    SiteSetting model created by singleton design pattern

--- site_settings/models.py
+++ site_settings/models.py
@@ -1,3 +1,76 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+from core.models import SingletonBaseModel
+from django.utils.translation import gettext_lazy as _
+
+
+class SiteSetting(SingletonBaseModel):
+    class Meta:
+        verbose_name = _('تنظیمات سایت')
+        verbose_name_plural = _('تنظیمات سایت')
+
+    site_name = models.CharField(
+        max_length=150,
+        verbose_name=_('نام سایت'),
+    )
+
+    phone = models.CharField(
+        max_length=11,
+        null=True,
+        blank=True,
+        verbose_name=_('تلفن ثایت ارتباطی'),
+        help_text=_('این شماره تلفن ثابت شماست. لطفا آن را با پیش شماره شهر خود وارد نمایید.'),
+        validators=[RegexValidator(
+            regex='^0\d{2,3}\d{8}$',
+            message=_('شماره تلفن ثابت معتبر نیست!')
+        )]
+    )
+
+    mobile_phone = models.CharField(
+        max_length=14,
+        null=True,
+        blank=True,
+        verbose_name=_('تلفن همراه ارتباطی'),
+        validators=[RegexValidator(
+            regex='^(0)?9\d{9}$',
+            message=_('شماره موبایل معتبر نیست!')
+        )]
+    )
+
+    email = models.EmailField(
+        verbose_name=_('آدرس ایمیل ارتباطی'),
+        null=True,
+        blank=True,
+    )
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name=_('توضیحات سایت'),
+        help_text=_('این توضیحات در بخش پاورقی وبسایت نمایش داده خواهند شد.'),
+    )
+
+    kavenegar_api_key = models.CharField(
+        max_length=150,
+        verbose_name=_('کد API key کاوه نگار'),
+        null=True,
+        blank=True,
+    )
+
+    kavenegar_sender = models.CharField(
+        max_length=150,
+        verbose_name=_('شماره اسال کننده پیامک کاوه نگار'),
+        null=True,
+        blank=True,
+    )
+
+    kavenegar_message = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name=_('متن پیامک کاوه نگار'),
+        help_text=_('هر متنی که وار کنید به همراه عبارت ؛کد تایید: xxxx به کاربر پیامک خواهد شد.')
+    )
+
+    def __str__(self):
+        return self.site_name
