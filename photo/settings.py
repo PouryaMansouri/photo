0f8dd41a9c4f6f4ebc522768d3449984a2387d55
kiarashfz <kiawfz3673@gmail.com>
2022-04-18 07:14:10 +0430

    jazzmin configs added

--- photo/settings.py
+++ photo/settings.py
@@ -12,6 +12,122 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "Salon Admin",
+
+    # Title on the brand, and login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "Salon",
+
+    # Welcome text on the login screen
+    "welcome_sign": "Welcome to Salon admin panel! :)",
+
+    # Copyright on the footer
+    "copyright": "KiarashFz",
+
+    # The model admin to search from the search bar, search bar omitted if excluded
+    "search_model": "user.User",
+
+    # Field name on user model that contains avatar image
+    "user_avatar": None,
+
+    # Links to put along the top menu
+    "topmenu_links": [
+
+        # Url that gets reversed (Permissions can be added)
+        {"name": "Home",  "url": "admin:index", "permissions": ["auth.view_user"]},
+
+        # model admin to link to (Permissions checked against model)
+        {"model": "user.User"},
+    ],
+
+    #############
+    # User Menu #
+    #############
+
+    # Additional links to include in the user menu on the top right ("app" url type is not allowed)
+    "usermenu_links": [
+        {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
+        {"model": "user.User"}
+    ],
+
+    # Whether to display the side menu
+    "show_sidebar": True,
+
+    # Whether to aut expand the menu
+    "navigation_expanded": True,
+
+    # Hide these apps when generating side menu e.g (auth)
+    "hide_apps": [],
+
+    # Hide these models when generating side menu (e.g auth.user)
+    "hide_models": ['reserve.WorkTime'],
+
+    # for the full list of 5.13.0 free icon classes
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "user.User": "fas fa-user",
+        "user.Mentor": "fas fa-chalkboard-teacher",
+        "user.Candidate": "fas fa-user-tie",
+        "voice.Voice": "fas fa-music",
+        "team.Team": "fas fa-users",
+        "score.ScoreItem": "fas fa-star-half-alt",
+        "auth.Group": "fas fa-users-cog",
+        "reserve.Agent": "fas fa-solid fa-address-card",
+        "reserve.Service": "fas fa-brands fa-stripe",
+        # "reserve.Holiday": "fas fa-regular fa-calendar-xmark",
+        "reserve.WorkTime": "fas fa-solid fa-clock",
+        "reserve.AgentWorkday": "fas fa-solid fa-business-time",
+        # "reserve.Workday": "fas fa-solid fa-calendar-days",
+        "reserve.Customer": "fas fa-solid fa-user",
+        "reserve.Booking": "fas fa-solid fa-hourglass",
+        # "reserve.AgentService": "fas fa-solid fa-hand-dots",
+    },
+    # Icons that are used when one is not manually specified
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
+
+    "related_modal_active": False,
+
+    "show_ui_builder": True,
+
+    "changeform_format": "horizontal_tabs",
+    "changeform_format_overrides": {"user.User": "collapsible"},
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-success",
+    "accent": "accent-purple",
+    "navbar": "navbar-navy navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": True,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": True,
+    "sidebar": "sidebar-dark-lime",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "cerulean",
+    "dark_mode_theme": "",
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +147,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
